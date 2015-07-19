a69b4db49089a49ce7bb9b1d8570857fbaad5a92
Xavier Serrano <zombispormedio007@gmail.com>
2015-07-19 20:20:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var Bot = require('node-telegram-bot');
+
+var bot = new Bot({
+  token: '123477263:AAFYEdXRp8nrrPvqXXWKyoaOqk7nOfvcEx4'
+})
+.on('message', function (message) {
+  if(message){
+    bot.sendMessage({chat_id:message.chat.id,text:"Pika Pika-Chu" }, function(err, res){
+      if(err){console.log(err); return;}
+      console.log(res);
+    });
+
+    bot.sendPhoto({chat_id:message.chat.id,
+      files:{ photo:"hello.png" }},
+      function(err, res){
+      if(err){console.log(err); return;}
+      console.log(res);
+    });
+
+
+  }
+})
+.start();
