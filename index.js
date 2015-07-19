3af07345840130dcc46b227f6136de78a633bc32
Xavier Serrano <zombispormedio007@gmail.com>
2015-07-19 21:10:57 +0200

    better

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var bot = new Bot({
 })
 .on('message', function (message) {
   if(message){
-    bot.sendMessage({chat_id:message.chat.id,text:"Pika Pika-Chu" }, function(err, res){
+  /*  bot.sendMessage({chat_id:message.chat.id,text:"Pika Pika-Chu" }, function(err, res){
       if(err){console.log(err); return;}
       console.log(res);
-    });
+    });*/
 
     bot.sendPhoto({chat_id:message.chat.id,
+      caption:"Pika Pika-Chu",
       files:{ photo:"hello.png" }},
       function(err, res){
       if(err){console.log(err); return;}
