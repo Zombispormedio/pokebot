b1380aa230d2c454f2df0617cf17069e2d8bd1e2
Xavier Serrano <zombispormedio007@gmail.com>
2015-07-25 18:46:30 +0200

    better

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
-var Bot = require('node-telegram-bot');
+//var Bot = require('node-telegram-bot');
 
 
 var express = require('express');
 var app = express();
-
+require("./config/express.js")(app);
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+var router=express.Router();
+app.use(router);
+
+var pika=express.Router();
+
+pika.route("/talk")
+    .get(function(req, res){
+    console.log(req.body);
+    res.send(200, "Perfect");
+})
+    .post(function(req, res){
+    console.log(req.body);
+    res.send(200, "Perfect");
+});
+
+app.use(pika);
 
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var bot = new Bot({
   token: '123477263:AAFYEdXRp8nrrPvqXXWKyoaOqk7nOfvcEx4'
 })
 .on('message', function (message) {
   if(message){
-  /*  bot.sendMessage({chat_id:message.chat.id,text:"Pika Pika-Chu" }, function(err, res){
+    bot.sendMessage({chat_id:message.chat.id,text:"Pika Pika-Chu" }, function(err, res){
       if(err){console.log(err); return;}
       console.log(res);
-    });*/
+    });
 
     bot.sendPhoto({chat_id:message.chat.id,
       caption:"Pika Pika-Chu",
@@ -31,10 +88,4 @@ var bot = new Bot({
 
   }
 })
-.start();
-
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+.start();*/
